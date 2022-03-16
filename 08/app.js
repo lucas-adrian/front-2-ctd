34666b71bbb16de8b4f8d2da3a826fd04ad7edb4
Willian Sant Anna <w.s.santanna@gmail.com>
2022-03-16 18:24:26 -0300

    Ajuste no código fonte da aula 08

--- 08/app.js
+++ 08/app.js
@@ -6,26 +6,47 @@ var item = document.querySelector("#elemento");
 // 1. Eventos do mouse...
 
 // 1.1. Click
-item.addEventListener("click", (evento) => evento.target.innerText = "Click" );
+item.addEventListener("click", function(evento) {
+    // item.innerText = "Click";
+    evento.target.innerText = "Click";
+});
 
 // 1.2. Doble click
-item.addEventListener("dblclick", (evento) => evento.target.innerText = "Doble Click" );
+item.addEventListener("dblclick", function(evento) {
+    // item.innerText = "Duplo Click";
+    evento.target.innerText = "Duplo Click";
+});
 
 // 1.3. Mouse over
-item.addEventListener("mouseover", (evento) => evento.target.style.backgroundColor = "red");
+item.addEventListener("mouseover", function(evento) {
+    evento.target.style.backgroundColor = "black";
+});
 
 // 1.4. Mouse out
-item.addEventListener("mouseout", (evento) => evento.target.style.backgroundColor = "orangered");
+function mudarCorDeFundo(evento) {
+    evento.target.style.backgroundColor = "orangered";
+}
+
+item.addEventListener("mouseout", mudarCorDeFundo);
 
 // ---------------------------------------------------
 
 // 2. Eventos do teclado...
 
-// 2.1. Key Down
-documento.addEventListener("keydown", () => item.innerText = "Key Down" );
+// 2.1. Key Down - É pressionada
+documento.addEventListener("keydown", function(evento) {
+    item.innerText = "Key Down";
+    console.log(evento.code);
+});
 
-// 2.2. Key Press
-documento.addEventListener("keypress", () => item.innerText = "Key Press" );
+// 2.2. Key Press - Está pressionada...
+documento.addEventListener("keypress", function(evento) {
+    item.innerText = "Key Press";
+    console.log(evento.code);
+});
 
-// 2.1. Key Up
-documento.addEventListener("keyup", () => item.innerText = "Key Up" );
+// 2.1. Key Up...
+documento.addEventListener("keyup", function(evento) {
+    item.innerText = "Key Up";
+    console.log(evento.code);
+});
